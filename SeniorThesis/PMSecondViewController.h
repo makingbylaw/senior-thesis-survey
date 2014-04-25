e58ef1da3590d3dde3b754bbb8ff438613f2ff2d
Paul Mason <paul@form1.co.nz>
2014-04-24 19:53:40 -0700

    Collection view etc

--- SeniorThesis/PMSecondViewController.h
+++ SeniorThesis/PMSecondViewController.h
@@ -8,6 +8,8 @@
 
 #import <UIKit/UIKit.h>
 
-@interface PMSecondViewController : UIViewController
+@interface PMSecondViewController : UIViewController<UICollectionViewDataSource, UICollectionViewDelegate>
+
+@property (nonatomic,strong) IBOutlet UICollectionView *collectionView;
 
 @end
--- SeniorThesis/PMSecondViewController.m
+++ SeniorThesis/PMSecondViewController.m
@@ -18,12 +18,34 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    [self.collectionView registerClass:[UICollectionViewCell class] forCellWithReuseIdentifier:@"ImageColors"];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
+{
+    return 1;
+}
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"ImageColors" forIndexPath:indexPath];
+
+    // Set the image
+    cell.backgroundColor = [UIColor greenColor];
+    //UIImageView *cellImage= (UIImageView*)[cell viewWithTag:85];
+    //cellImage.image = [UIImage imageNamed:@"paul.jpg"];
+    return cell;
+}
+
+- (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    return CGSizeMake(50, 50);
 }
 
 @end
